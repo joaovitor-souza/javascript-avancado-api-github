e9643859c2a5cd2bf3ca7906550c8fcf0210291a
Joao Vitor <joaovitorfesouza@gmail.com>
2023-05-04 17:12:58 -0300

    Corrigido caminho dos arquivos

--- src/script/services/events.js
+++ src/script/services/events.js
@@ -1,5 +1,5 @@
-import { baseUrl } from "/src/script/variables.js";
-import { repositoriesQuantity } from "/src/script/variables.js"
+import { baseUrl } from "../variables.js";
+import { repositoriesQuantity } from "../variables.js"
 
 async function getEvents(userName) {
   const response = await  fetch(`${baseUrl}/${userName}/events?per_page=${repositoriesQuantity}`);
