686f498ba71a4fd36483bcc91bdc066cf4ca5501
Joao Vitor <joaovitorfesouza@gmail.com>
2023-05-04 14:33:21 -0300

    Adicionado mensagem aos itens para caso o valor retornado seja nulo ou igual a 0

--- src/script/objects/screen.js
+++ src/script/objects/screen.js
@@ -1,67 +1,83 @@
 const screen = {
-    userProfile: document.querySelector(".profile-data"),
-    renderUser(user){
-        this.userProfile.innerHTML = `<div class="info">
-                                 <img src="${user.avatarUrl}" alt="Foto do perfil do usário" />
-                                 <div class="data">
-                                    <h1>${user.name ?? "Não possui nome cadastrado 😥:("}</h1>
-                                    <p>${user.bio ?? "Não possui bio cadastrada 😥"}</p>
-                                    <p><span>👥Seguidores: </span>${user.followers}</p>
-                                    <p><span>👥Seguindo: </span>${user.following}</p>
-                                </div> 
-                            </div>`
-        let repositoriesItens = ''
+  userProfile: document.querySelector(".profile-data"),
+  renderUser(user) {
+    this.userProfile.innerHTML = 
+            `<div class="info">
+                <img src="${user.avatarUrl}" alt="Foto do perfil do usário" />
+                <div class="data">
+                    <h1>${user.name ?? "Não possui nome cadastrado 😥:("}</h1>
+                    <p>${user.bio ?? "Não possui bio cadastrada 😥"}</p>
+                    <p><span>👥Seguidores: </span>${user.followers}</p>
+                    <p><span>👥Seguindo: </span>${user.following}</p>
+                </div> 
+            </div>`;
 
-        if(user.repositories.length > 0){
+    let repositoriesItens = "";
 
-            user.repositories.forEach(repo => repositoriesItens += 
-           `<li>
+    if (user.repositories.length > 0) {
+      user.repositories.forEach(
+        (repo) =>
+          (repositoriesItens += 
+            `<li>
                 <a href="${repo.html_url}" target="_blanck">
                     <h3>${repo.name}</h3><br>
-                    <span> 🍴${repo.forks}</span>
-                    <span>👀${repo.watchers}</span>
-                    <span>⭐${repo.stargazers_count}</span> 
-                    <span>👨‍💻${repo.language}</span>
+                    <span> 🍴${
+                      repo.forks === 0 ? "Sem forks" : repo.forks
+                    }</span>
+                    <span>👀${
+                      repo.watchers === 0 ? "Sem watchers" : repo.watchers
+                    }</span>
+                    <span>⭐${
+                      repo.stargazers_count === 0
+                        ? "Sem estrelas"
+                        : repo.stargazers_count
+                    }</span> 
+                    <span>👨‍💻${repo.language ?? "Sem Linguagens"}</span>
                 </a>
-            </li>`)  
-            this.userProfile.innerHTML += `<div class="repositories section">
-                                                <h2>Repositórios</h2>
-                                                <ul>${repositoriesItens}</ul>
-                                            </div>`
-        }
-        
-        
-        let eventsList = '';
-        user.events.forEach(event => {
-        let message = '';
-
-        if (event.type === 'CreateEvent') {
-            message = 'Sem mensagem';
-        } else if (event.type === 'PushEvent' && event.payload.commits.length > 0) {
-            event.payload.commits.forEach(commit => {
-            message += `<span>${commit.message}</span>`;
-            });
-        }
-        eventsList += `
-            <li class ="events">
-            <p>${event.repo.name} - </p>
-            <span>${message}</span>
-            </li>
-        `;
-        });                                     
-
-
-        this.userProfile.innerHTML +=
-                                        `<div class="section">
-                                            <h2>Eventos</h2>
-                                                <ul>${eventsList}</ul>
-                                            </div>`
-
+            </li>`)
+      );
+      this.userProfile.innerHTML += 
+            `<div class="repositories section">
+                <h2>Repositórios</h2>
+                <ul>${repositoriesItens}</ul>
+            </div>`;
     }
-    ,
-    renderNotFound(){
-        this.userProfile.innerHTML = "<h3>Usuário não encontrado</h3>"
-    },
-}
 
-export { screen }
+    let eventsItens = '';
+    user.events.forEach(element =>{
+      if (element.type === 'PushEvent'){
+          eventsItens += 
+          `<li class ="events">
+            <h3>${element.repo.name}</h3>
+            <p> -- ${element.payload.commits[0].message}</p>
+          </li>`
+      }else{
+        eventsItens += 
+        `<li class="events">
+            <h3>${element.repo.name}</h3>
+            <p> -- Criado um  ${element.payload.ref_type}</p>
+          </li>`
+      }
+    })
+
+    if(user.events.length === 0) {
+      this.userProfile.innerHTML += `
+      <div class="section">
+          <h2>Eventos</h2>
+          <h3>Não há eventos</h3>
+      </div>`
+      } 
+      else{
+        this.userProfile.innerHTML += 
+        `<div class="section">
+            <h2>Eventos</h2>
+            <ul>${eventsItens}</ul>
+        </div>`;
+      }
+      
+  },
+  renderNotFound() {
+    this.userProfile.innerHTML = "<h3>Usuário não encontrado</h3>";
+  },
+};
+export { screen };
