8ca547e0d72103b5dcb4149301889a6512151d8c
Joao Vitor <joaovitorfesouza@gmail.com>
2023-05-04 14:36:22 -0300

    Adicionado os métodos filter e slice para busca personalizada dos eventos

--- src/script/services/events.js
+++ src/script/services/events.js
@@ -2,7 +2,9 @@ import { baseUrl } from "/src/script/variables.js";
 import { repositoriesQuantity } from "/src/script/variables.js"
 
 async function getEvents(userName) {
-  const response = await fetch(`${baseUrl}/${userName}/events?per_page=${repositoriesQuantity}`);
-  return await response.json();
+  const response = await  fetch(`${baseUrl}/${userName}/events?per_page=${repositoriesQuantity}`);
+  const events = await response.json()
+  return events.filter(element => element.type === 'CreateEvent' || element.type === 'pushEvent').slice(0, repositoriesQuantity)
+
 }
 export { getEvents };
